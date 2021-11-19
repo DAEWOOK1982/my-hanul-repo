e970180aa319e8f22d393272d83fd2ad56b1e3cc
DEAWOOK <xcellenceo@gmail.com>
2021-11-19 17:56:10 +0900

    20211119 jsp 수업내용

---
+++ study_web/04.TestMVC/WebContent/WEB-INF/classes/com/hanul/study/book.sql
@@ -0,0 +1,14 @@
+-- 테이블 생성
+create table tblBook(
+  title 	varchar2(30),
+  name 	  	varchar2(20),
+  isbn 	  	varchar2(20) primary key not null,
+  comp 	  	varchar2(20),
+  cost 	  	number,
+  qty		number,
+  price 	number
+);
+
+-- 전체레코드 검색
+select * from tblBook;
+
