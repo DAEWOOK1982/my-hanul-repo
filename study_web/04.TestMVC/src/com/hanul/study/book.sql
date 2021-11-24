5cb927d39dfd4658718f84c6dab194052fd5876b
DAEWOOK <xcellenceo@gmail.com>
2021-11-25 00:33:19 +0900

    web수업 과제 home

--- study_web/04.TestMVC/src/com/hanul/study/book.sql
+++ study_web/04.TestMVC/src/com/hanul/study/book.sql
@@ -2,7 +2,7 @@ create table tblBook(
      title varchar2(30),
      name VARCHAR(20),
      isbn VARCHAR(20) PRIMARY KEY not null,
-     com  VARCHAR(20),
+     comp  VARCHAR(20),
      cost number,
      qty  number,
      price number
@@ -10,5 +10,3 @@ create table tblBook(
 
 select *
 from tblbook;
-
-alter table tblbook rename column com to comp;
