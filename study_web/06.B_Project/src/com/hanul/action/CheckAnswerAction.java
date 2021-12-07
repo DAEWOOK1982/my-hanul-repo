1843be9f90be8c75de71e3dd8a971f9e82497048
DAEWOOK <xcellenceo@gmail.com>
2021-12-07 09:02:26 +0900

    project 1206 home

---
+++ study_web/06.B_Project/src/com/hanul/action/CheckAnswerAction.java
@@ -0,0 +1,19 @@
+package com.hanul.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CheckAnswerAction implements Action {
+	@Override
+	public ActionForward excute(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		//클라이언트의 요청을 받는다.
+		
+		
+		return null;
+	}
+
+}
---
+++ study_web/06.B_Project/src/com/hanul/dto/TestDTO.java
@@ -0,0 +1,10 @@
+package com.hanul.dto;
+
+import java.io.Serializable;
+
+public class TestDTO implements Serializable {
+	private String subject;
+	private int answer1, answer2, answer3, answer4, answer5, 
+				answer6, answer7, answer8, answer9, answer10;
+	
+}
